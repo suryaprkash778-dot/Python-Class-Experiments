052c6b51e245b82c88d31c5dd8cbcb863b54351d
Surya Prakash Sharma <Surya.27399@stu.upes.ac.in>
2026-03-02 05:21:46 +0530

    Update eperiment5.py

--- eperiment5.py
+++ eperiment5.py
@@ -1,4 +1,4 @@
-# WAP to input a list of scores for N students in a list data type. Find the score of the runner-up and print the output.
+#3 WAP to input a list of scores for N students in a list data type. Find the score of the runner-up and print the output.
 
 n = int(input("Enter number of students: "))
 scores = list(map(int, input("Enter scores: ").split()))
@@ -13,7 +13,76 @@ for i in scores:
 runner_up = max(new_list)
 print("Runner-up score:", runner_up)
 
-# Create a Todo list Manager where users can add, view, and remove tasks. Use List for storing tasks.
+#4.	Create a dictionary of n persons where key is name and value is city.
+students = {
+    "surya":"jaipur",
+    "praneesh":"mumbai",
+    "divay":"karnal",
+    "ved":"pune",
+    "saxena":"bareli"
+}
+#a) Display all names
+for x in students:
+    print(x)
+
+#b) Display all city names
+print("\n")
+for x in students:
+    print(students[x])
+
+#c) Display student name and city of all students.
+for x,y  in students.items():
+    print(x,y)
+
+#d) Count number of students in each city.
+print("\nNumber of Students in Each City:")
+city_count = {}
+for city in students.values():
+    city_count[city] = city_count.get(city, 0) + 1
+
+for city, count in city_count.items():
+    print(f"{city}: {count}")
+
+#5.	Store details of n movies in a dictionary by taking input from the user.
+#Each movie must store details like name,  year, director name, production cost, collection made (earning) & perform the following :-
+movies ={}
+n = int(input("enter number of movies\n"))
+for i in range(n):
+    print(f"\nEnter details for movie {i + 1}:")
+    name = input("Movie Name: ")
+    year = int(input("Release Year: "))
+    director = input("Director Name: ")
+    production_cost = float(input("Production Cost: "))
+    collection = float(input("Collection Made : "))
+
+    movies[name] = {
+        "year": year,
+        "director": director,
+        "production_cost": production_cost,
+        "collection": collection
+    }
+#a)	print all movie details
+for name, details in movies.items():
+    print(f"{name} → {details}")
+
+#b)	display name of movies released before 2015
+for name, details in movies.items():
+    if details["year"] < 2015:
+        print(name)
+
+#c)	print movies that made a profit.
+for name, details in movies.items():
+        if details["collection"]-details["production_cost"] > 0:
+            print(name)
+
+#d)	print movies directed by a particular director.
+director_name = input("\nEnter director name to search: ")
+for name, details in movies.items():
+    if details["director"].lower() == director_name.lower():
+        print(name)
+
+
+#7 Create a Todo list Manager where users can add, view, and remove tasks. Use List for storing tasks.
 
 tasks = []
 
